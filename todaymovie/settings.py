df6866ff00924896345f6e05b356f29300fab0c6
한상민 <tkdalsgks@hansangmin-ui-MacBookAir.local>
2021-10-05 14:12:15 +0900

    캐시 삭제

--- todaymovie/settings.py
+++ todaymovie/settings.py
@@ -20,14 +20,13 @@ env = environ.Env(
     DEBUG=(bool, False)
 )
 
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 # reading .env file
 environ.Env.read_env(
     env_file=os.path.join(BASE_DIR, '.env')
 )
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
