07608a55f42d1d219f217bce28f065db53ad039f
한상민 <tkdalsgks@hansangmin-ui-MacBookAir.local>
2021-10-15 00:35:51 +0900

    검색 기능 1차

--- todaymovie/urls.py
+++ todaymovie/urls.py
@@ -18,10 +18,10 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-from articleapp.views import ArticleListView
+from todaymovieapp.views import main
 
 urlpatterns = [
-    path('', ArticleListView.as_view(), name='home'),
+    path('', main, name='home'),
 
     path('admin/', admin.site.urls),
     path('todaymovies/', include('todaymovieapp.urls')),
